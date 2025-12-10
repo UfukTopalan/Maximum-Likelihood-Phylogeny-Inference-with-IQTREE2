ed28c2ac878a24f6e856594dc502563b1c3bf26c
Ufuk Topalan <double123416@gmail.com>
2025-12-10 11:13:26 +0200

    Create contact_segul.sh

---
+++ scripts_folder/contact_segul.sh
@@ -0,0 +1,5 @@
+# Concatenation
+  #cd $HOME/Rdata # in Linux
+  cd /mnt/c/Rdata # in Ubuntu on Windows
+  segul align concat -i *_concat.fas --force -F fasta
+  cp Align-Concat/Align-Concat.fas temp_in.fas
